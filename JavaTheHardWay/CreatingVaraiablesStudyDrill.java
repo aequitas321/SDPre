61fc152fe772e6f1acd7e83c5666e146caa38564
Shane Duncan <aequitas321@icloud.com>
2020-02-25 12:22:25 -0500

    LJTHW Exercise5 Study Drill

---
+++ JavaTheHardWay/CreatingVaraiablesStudyDrill.java
@@ -0,0 +1,32 @@
+public class CreatingVaraiablesStudyDrill {
+ public static void main( String[] args ) {
+ int x, y, age, daughterAge;
+ double seconds, e, checking, timeMarried;
+ String firstName, lastName, title, wife, daughter;
+
+ x = 10;
+ y = 400;
+ age = 39;
+ daughterAge = 1;
+
+ seconds = 4.71;
+ e = 2.71828182845904523536;
+ checking = 1.89;
+ timeMarried = 2.5;
+
+ firstName = "Graham";
+ lastName = "Mitchell";
+ title = "Mr.";
+ wife = "Natasha";
+ daughter = "Emery";
+
+ System.out.println( "The variable x contains " + x );
+ System.out.println( "The value " + y + " is stored in the variable y." );
+ System.out.println( "The experiment took " + seconds + " seconds." );
+ System.out.println( "A favorite irrational # is Euler's number: " + e );
+ System.out.println( "Hopefully you have more than $" + checking + "!" );
+ System.out.println( "My name's " + title + " " + firstName + lastName );
+ System.out.println("My wife's name is " + wife + " and my daughter is " + daughter +
+  ". She is " + daughterAge + ". My wife and I have been married " + timeMarried + " years.");
+    }
+ }
