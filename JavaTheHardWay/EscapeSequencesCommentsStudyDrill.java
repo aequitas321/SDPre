668b505049b2f8a58a496301116c905d2f849b7e
Shane Duncan <aequitas321@icloud.com>
2020-02-25 10:21:37 -0500

    LJTHW Exercise4 Study Drills

---
+++ JavaTheHardWay/EscapeSequencesCommentsStudyDrill.java
@@ -0,0 +1,34 @@
+public class EscapeSequencesCommentsStudyDrill {
+ public static void main( String[] args ) {
+ // This exercise demonstrates escape sequences & comments (like these)!
+ System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" +
+  "\tLearn Java the \"Hard\" Way!\n" +
+ // System.out.frimp( "Learn Java the Hard Way" );
+  "Hello\n" + // This line prints Hello.
+  "Jello\by\n" + // This line prints Jelly.
+ /* The quick brown fox jumped over a lazy dog.
+ Quick wafting zephyrs vex bold Jim. */
+  /* testing */  "Hard to believe, eh?\n" +
+  "Surprised? /* abcde */ Or what did you expect?\n" +
+  "\\ // -=- \\ //\n" +
+  "\\\\ \\\\\\ \\\\\\\\\n" + // it takes 2 to make 1
+  "I hope you understand \"escape sequences\" now.\n" );
+ // and comments. :)
+    }
+ }
+
+
+
+ //
+ // Learn	Java
+ // 	the
+ // Hard	Way
+ //
+ // 	Learn Java the "Hard" Way!
+ // Hello
+ // Jelly
+ // Hard to believe, eh?
+ // Surprised? /* abcde */ Or what did you expect?
+ // \ // -=- \ //
+ // \\ \\\ \\\\
+ // I hope you understand "escape sequences" now.
