a7281f8b50dc8996a2396a126ba659c337738eab
Shane Duncan <aequitas321@icloud.com>
2020-02-25 13:49:35 -0500

    SD Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Shane Duncan";
+    int age = 31;
+    String state = "Kentucky";
+
+    System.out.print("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
