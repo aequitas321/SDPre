73e6f6c08cf51413339b37efb44a792631677dad
Shane Duncan <aequitas321@icloud.com>
2020-02-24 23:12:02 -0500

    GasolineReceiptStudyDrill

---
+++ JavaTheHardWay/GasolineReceiptStudyDrill.java
@@ -0,0 +1,16 @@
+public class GasolineReceiptStudyDrill {
+  public static void main(String[] args){
+    System.out.println("+------------------------+");
+    System.out.println("|                        |");
+    System.out.println("|       CORNER STORE     |");
+    System.out.println("|                        |");
+    System.out.println("| 2020-02-24   04:30PM   |");
+    System.out.println("|                        |");
+    System.out.println("| Gallons:        10.870 |");
+    System.out.println("| Price/gallon:  $ 2.079 |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:   $ 22.60  |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}
