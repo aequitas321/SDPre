c833056842ccfe3297a69dea9d5f206eedb8e42a
Shane Duncan <aequitas321@icloud.com>
2020-02-25 22:47:07 -0500

    SD Objective3Lab5

---
+++ Labs/Objective3Lab5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective3Lab5{
+  public static void main(String[] args){
+    double num1, num2, sumOfNum;
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Please enter a number: ");
+    num1 = keyboard.nextDouble();
+
+    System.out.println("Please enter another number: ");
+    num2 = keyboard.nextDouble();
+
+    sumOfNum = num1 + num2;
+
+    System.out.println("The sum of " + num1 + " + " + num2 + " = " + sumOfNum);
+  }
+}
