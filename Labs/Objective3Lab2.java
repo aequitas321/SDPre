217861370b73a5a4c0780f5b6a27e150bb7da369
Shane Duncan <aequitas321@icloud.com>
2020-02-25 22:08:53 -0500

    SD Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Objective3Lab2 {
+  public static void main(String[] args){
+    String name;
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("What is your name?");
+    name = keyboard.next();
+    System.out.println("Hello " + name);
+  }
+}
