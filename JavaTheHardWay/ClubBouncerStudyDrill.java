2dcd646561ccb895691a920ecc64e1ffbffb95d8
Shane Duncan <aequitas321@icloud.com>
2020-02-26 17:45:16 -0500

    LJTHW Exercise17 StudyDrill

---
+++ JavaTheHardWay/ClubBouncerStudyDrill.java
@@ -0,0 +1,18 @@
+public class ClubBouncerStudyDrill {
+    public static void main(String[] args) {
+        int age = 22;
+        boolean onGuestList = false;
+        double allure = 7.5;
+        String gender = "F";
+
+        if (onGuestList || age >= 21 || (gender.equals("F") && allure >= 8)) {
+            System.out.println("You are allowed to enter the club.");
+        }
+        System.out.println("BAM!");
+        else {
+            System.out.println("You are not allowed to enter the club.");
+        }
+    }
+}
+
+//Does not compile because the if statment has been broken.
