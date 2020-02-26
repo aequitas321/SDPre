5ee6ae4b56b90c795b405d2df29fb4984d1bce65
Shane Duncan <aequitas321@icloud.com>
2020-02-25 19:23:55 -0500

    LJTHW Exercise1 StudyDrill

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,5 +1,5 @@
 public class FirstProg{
-  public static void main(String[] args){
+  public static void main(String ... args){
     System.out.println("I, Shane Duncan, am determined to learn how to code.");
     System.out.println("Toady's date is Monday, Feb. 24th 2020.");
   }
