e2976c1aeed5324ef84ae2ecd70409f395161a06
Shane Duncan <aequitas321@icloud.com>
2020-02-29 01:16:28 -0500

    SD Objective6Lab5

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Objective6Lab5 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int selection;
+
+        while (true) {
+            System.out.println("_____Menu_____");
+            System.out.println("1: Say Hello");
+            System.out.println("2: List My favorite foods");
+            System.out.println("3: Exit");
+            System.out.println();
+
+            selection = keyboard.nextInt();
+
+            if (selection == 1) {
+                System.out.println("Hello human");
+            } else if (selection == 2) {
+                System.out.println("Apples, Bananas, Coconuts");
+            } else if (selection == 3) {
+                break;
+            }
+        }
+        System.out.println("Goodbye");
+
+    }
+}
