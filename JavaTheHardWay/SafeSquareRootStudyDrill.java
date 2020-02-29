0c5f45adfc326f6ca5bfe73bfd91d6025f682aad
Shane Duncan <aequitas321@icloud.com>
2020-02-28 21:06:14 -0500

    LJTHW Exercise28 StudyDrill

---
+++ JavaTheHardWay/SafeSquareRootStudyDrill.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SafeSquareRootStudyDrill {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        double x, y;
+        String ready;
+
+        System.out.println("Are you ready? ");
+        ready = keyboard.next();
+        ready = ready.toLowerCase();
+
+        while(! ready.equals("yes")) {
+            System.out.println("Let's get ready!");
+            System.out.println("Are you ready?");
+            ready = keyboard.next();
+            ready = ready.toLowerCase();
+        }
+
+        System.out.print("Give me a number, and I'll find it's square root. ");
+        System.out.print("(No negatives, please.) ");
+        x = keyboard.nextDouble();
+
+        while (x < 0) {
+            System.out.println("I won't take the square root of a negative.");
+            System.out.print("\nNew number: ");
+            x = keyboard.nextDouble();
+        }
+
+        y = Math.sqrt(x);
+
+        System.out.println("The square root of " + x + " is " + y);
+    }
+}
